03d4d03b8f22941be77f106cb0a69f394a3e3e27
Gustavo Ottoboni <ottoguz87@gmail.com>
2023-07-28 09:22:32 -0300

    Update: Made login() function async

--- desafio-think-frontend/src/app/components/login/login.component.ts
+++ desafio-think-frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public users: any;
+  constructor(private http: HttpClient) {
 
+  }
+  ngOnInit(): void {
+    try {
+      this.login();
+    } catch {
+      throw new Error('Method not implemented.');
+    }
+  }
+
+  public async login() {
+    const header = new HttpHeaders({
+      contentType: 'application/json'
+    })
+    let body = {
+      email: 'eve.holt@reqres.in',
+      password: 'cityslicka',
+    }
+    await this.http.post('https://reqres.in/api/login', body, {headers: header}).subscribe((data) => {
+      console.log(data);
+      this.users = data;
+    });
+  }
 }
