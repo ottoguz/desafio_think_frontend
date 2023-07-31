1c29d6d65dd04be857d8a78df2668a2ce75b86f0
Gustavo Ottoboni <ottoguz87@gmail.com>
2023-07-31 16:02:33 -0300

    Update: feture color choose /home created

--- desafio-think-frontend/src/app/app.module.ts
+++ desafio-think-frontend/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
-import { HttpClientModule} from '@angular/common/http'
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
---
+++ desafio-think-frontend/src/assets/js/change-paint.js
@@ -0,0 +1,23 @@
+function changePaintBlue400() {
+    document.getElementById('paint').style.backgroundColor = "#60a5fa";
+}
+
+function changePaintRed950() {
+    document.getElementById('paint').style.backgroundColor = "#450a0a";
+}
+
+function changePaintYellow600() {
+    document.getElementById('paint').style.backgroundColor = "#ca8a04";
+}
+
+function changePaintGreen600() {
+    document.getElementById('paint').style.backgroundColor = "#16a34a";
+}
+
+function changePaintAmber700() {
+    document.getElementById('paint').style.backgroundColor = "#b45309";
+}
+
+function changePaintPurple400() {
+    document.getElementById('paint').style.backgroundColor = "#c084fc";
+}
